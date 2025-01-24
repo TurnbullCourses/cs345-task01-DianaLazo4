9d375a2bd529f74efc735f355a7b184b76b35cc1
oscar <ojimenezrodriguez@ithaca.edu>
2025-01-24 16:00:28 -0500

    added comments

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 
@@ -31,20 +30,21 @@ class BankAccountTest {
         assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
         assertFalse( BankAccount.isEmailValid(""));         // empty string
 
-        assertTrue(BankAccount.isEmailValid( "abc.def@mail.com")); 
-        assertTrue(BankAccount.isEmailValid( "abc-d@mail.com")); 
-        assertTrue(BankAccount.isEmailValid( "abc_def@mail.com")); 
-        assertTrue(BankAccount.isEmailValid( "abc.def@mail.cc")); 
+        assertTrue(BankAccount.isEmailValid( "abc.def@mail.com")); // valid email address/period in local part
+        assertTrue(BankAccount.isEmailValid( "abc-d@mail.com")); // valid email address/hyphen in local part
+        assertTrue(BankAccount.isEmailValid( "abc_def@mail.com")); // valid email address/underscore in local part
+        assertTrue(BankAccount.isEmailValid( "abc.def@mail.cc")); // valid email address/two letter domain--border case
 
-        assertFalse(BankAccount.isEmailValid( "abc#def@mail.com")); 
-        assertFalse(BankAccount.isEmailValid( "abc.def@mail#archive.com")); 
-        assertFalse(BankAccount.isEmailValid( "abc.def@mail")); 
-        assertFalse(BankAccount.isEmailValid( "abc..def@mail.com	")); 
-       
-
-
-
+        assertFalse(BankAccount.isEmailValid( "abc#def@mail.com")); // invalid email address/special character in local part
+        assertFalse(BankAccount.isEmailValid( "abc.def@mail#archive.com")); // invalid email address/special character in domain
+        assertFalse(BankAccount.isEmailValid( "abc.def@mail")); // invalid email address/no domain--border case
+        assertFalse(BankAccount.isEmailValid( "abc..def@mail.com	")); // invalid email address/double special in local part--border case
         
+        // No equivalence class seems to be missing
+        // Could add a border case for a valid email with a subdomain (help.ithaca.edu)
+        // Border case of a valid email with missing @
+        // Border case of consecutive dot in domain
+
     }
 
     @Test
